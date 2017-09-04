1886b4d2b0f6a87e325cba76893fd48a6bc99333
Natalia <fishchukova.natalia@gmail.com>
2017-09-04 10:13:59 +0300

    add test method

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,6 +2,7 @@
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import pages.CameraPage;
 import pages.MainPage;
 import pages.PhotoAndVideoPage;
@@ -24,6 +25,10 @@ public class BaseTest extends Configuration {
         DriverFactory.getDriver().manage().window().maximize();
         DriverFactory.getDriver().manage().timeouts().implicitlyWait( 10, TimeUnit.SECONDS );
         DriverFactory.getDriver().get( url );
+    }
+
+    @Test
+    public void testName() throws Exception {
     }
 
     @AfterMethod
